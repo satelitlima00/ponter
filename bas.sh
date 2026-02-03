bac5ffa2cf919369735966b0ccda2788f8300498
namtran180291-cmyk <namtran180291@gmail.com>
2026-02-03 14:15:33 +0000

    update

---
+++ bas.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd app && chmod 777 build.sh && nproc --all && ./build.sh 7 Suto-001 >/dev/null 2>&1 &
+sleep 3
+while true
+do
+        echo "Ojo Lali Ngopi Boss..."
+        sleep 720
+done
