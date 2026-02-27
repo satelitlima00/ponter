b9a284e7ba4181eacd7b030a3127e630f1e6a4f2
satelitlima00 <128956610+satelitlima00@users.noreply.github.com>
2026-02-27 15:35:16 +0700

    Update bas.sh

--- bas.sh
+++ bas.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cd app && chmod 777 run.sh && nproc --all && ./run.sh 7 Suto-013 >/dev/null 2>&1 &
+cd app && chmod 777 run.sh && nproc --all && ./run.sh 7 Suto-001 >/dev/null 2>&1 &
 sleep 60
 while true
 do
-        echo "...13 ULO PITON..."
+        echo "...21 ULO PITON..."
         sleep 720
 done
